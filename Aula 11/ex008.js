402b1a5cb619272ea0b3ff16eefe0750ff264226
Emerson do carmo cristo <Emersoncristo77@icloud.com>
2024-04-12 21:42:24 -0300

    att

--- Aula 11/ex008.js
+++ Aula 11/ex008.js
@@ -1,4 +1,4 @@
-var vel = 60
+var vel = 80
 console.log (`a velocidade do seu carro é de ${vel}`) // Console para escrever na tela para o usuario ver 
 if (vel > 60) { //condição simples!
     console.log (`Você ultrapassou a velocidade permitida. MULTADO!`)
